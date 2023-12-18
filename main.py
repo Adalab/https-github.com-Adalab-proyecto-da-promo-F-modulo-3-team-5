ef705bca6849b449eb5ca0aacd8f6c5632762477
Anabella Fontana <fontanaanabella@gmail.com>
2023-12-18 20:01:22 +0100

    ETL I finalizado

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
 #%%
 import pandas as pd
-import os
+import numpy as np
 
 import sys
+sys.path.append("../")
 from src import soporte as sp
-# %%
 # %%
 df= sp.abrir_archivo('../HR RAW DATA.csv')
 # %%
 eda = sp.exploracion_dataframe(df)
+# %%
+columnas_eliminar = ['roledepartament','numberchildren','over18', 'sameasmonthlyincome','employeecount','salary','standardhours', 'yearsincurrentrole']
+#%%
+df = sp.limpieza_columnas(df, df.columns.to_list(),columnas_eliminar )
+# %%
+columnas_limpiar = ['dailyrate','employeenumber','monthlyincome','performancerating','totalworkingyears','worklifebalance','distancefromhome']
+#info gral a minusculas
+columnas_minusculas=['age', 'attrition', 'businesstravel', 'department','education', 'educationfield','environmentsatisfaction', 'gender', 'hourlyrate', 'jobinvolvement',
+       'joblevel', 'jobrole', 'jobsatisfaction', 'maritalstatus', 'monthlyrate', 'numcompaniesworked', 'overtime',
+       'percentsalaryhike', 'relationshipsatisfaction','stockoptionlevel', 'trainingtimeslastyear', 'worklifebalance',
+       'yearsatcompany', 'yearssincelastpromotion','yearswithcurrmanager', 'yearbirth', 'remotework', 'dailyrate','totalworkingyears']
+# %%
+sp.limpieza_datos(df, columnas_limpiar, columnas_minusculas)
+# %%
+df['age'] = df['age'].apply(sp.convertir_a_numero)
+# %%
+df['gender'] = df['gender'].replace({0: 'male', 1: 'female'})
+df['gender'].value_counts()
+#%%
+df['maritalstatus']=df['maritalstatus'].replace({'marreid':'married'})
+#%%
+df['remotework']= df['remotework'].replace({('1'):'yes',('0') or ('false'): 'no', 'true': 'yes', 'false': 'no'})
+df['remotework'].value_counts()
+#%%
+df["distancefromhome"] = df["distancefromhome"].apply(lambda x: np.nan if x < 0 else x)
+
+#%%
+sp.eliminar_duplicados(df, 'employeenumber')
+
+#%%
+categorias_educacion = {1: 'high school', 2: 'college', 3: 'university', 4:'master degree', 5: 'phd'}
+categorias_joblevel = {1: 'entry', 2: 'intermediate', 3: 'experienced', 4: 'advanced', 5: 'expert'}
+
+sp.transformacion_datos(df, 'education', categorias_educacion)
+sp.transformacion_datos(df, 'joblevel', categorias_joblevel)
+
+#%%
+df['hourlyrate'] = df['hourlyrate'].replace( 'not available', 'unknown')
 # %%
--- src/soporte.py
+++ src/soporte.py
@@ -13,14 +13,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 import os
-import sys
-import sys
+#import sys
 
-from dotenv import load_dotenv
-load_dotenv()
-sys.path.append("../")
-# %%
 
+#from dotenv import load_dotenv
+#load_dotenv()
+#sys.path.append("../")
 # %%
 #funcion para abrir csv
 def abrir_archivo(nombre):
@@ -50,33 +48,105 @@ def exploracion_dataframe(dataframe):
         print(f"La columna {col.upper()} tiene las siguientes valores únicos:")
         display(pd.DataFrame(dataframe[col].value_counts()).head())    
 # %%
-def limpieza_datos (dataframe):
+def limpieza_columnas (dataframe,columnas,columnas_drop):
     #Homogeneizamos el nombre de las columnas 
-    nuevas_columnas = {columna: columna.lower() for columna in df_ejemplo.columns}
+    nuevas_columnas = {columna: columna.lower() for columna in dataframe.columns}
     dataframe.rename(columns=nuevas_columnas, inplace= True)
-
+    #renombramos una columna
     dataframe.rename(columns = {"datebirth": "yearbirth"}, inplace = True )
-    dataframe.drop(['roledepartament','numberchildren','over18', 'sameasmonthlyincome','employeecount','salary','standardhours', 'yearsincurrentrole'],axis = 1, inplace=True)
+    #eliminamos columnas
+    dataframe.drop(columnas_drop,axis = 1, inplace=True)
+ 
+
+    #pasamos a minuscula el nombre de las columnas
+    for columna in columnas:
     
-    # 
-    columnas_limpiar = ['dailyrate','employeenumber','monthlyincome','performancerating','totalworkingyears','worklifebalance','distancefromhome']
+        try:
+            dataframe[columna]=dataframe[columna].str.lower()
+        except:
+            pass
+    
+    return dataframe
+    
+# %%
+def limpieza_datos (dataframe,columnas_limpiar, columnas_minusculas):
+    
     for columna in columnas_limpiar:
         try:
-            df_ejemplo[columna] =(df_ejemplo[columna].str.replace('$', ''))
+            dataframe[columna] =(dataframe[columna].str.replace('$', ''))
         
         except:
             pass
         
         try:
-            df_ejemplo[columna] =(df_ejemplo[columna].str.replace(',', '.')) 
+            dataframe[columna] =(dataframe[columna].str.replace(',', '.')) 
 
         except:
             pass
             
         try:
-          df_ejemplo[columna]=pd.to_numeric(df_ejemplo[columna]).astype(float)
+          dataframe[columna]=pd.to_numeric(dataframe[columna]).astype(float)
         except:
           pass
+       
+   
+    for columna in columnas_minusculas:
+    
+        try:
+            dataframe[columna]=dataframe[columna].str.lower()
+        except:
+            pass
+    
     
     return dataframe
-    
+# %%
+#Para columna de Age
+
+def convertir_a_numero(valor):
+    if isinstance(valor, int): # Verifica si el valor ya es un entero, si lo es no hace nada
+        return valor
+    elif isinstance(valor, str): # Intenta convertir el valor a número
+        try: 
+            numero = int(valor)
+            return numero
+        except ValueError:
+            try: # aquí es donde se usa la función w2n en el try pq si la conversion falla entonces se usará
+                numero = w2n.word_to_num(valor)
+                return numero
+            except ValueError:
+                print(f"Error: No se pudo convertir '{valor}' a número.")
+                return None
+    else:
+        print(f"Error: Tipo de dato no soportado para el valor '{valor}'.")
+        return None
+
+# %%
+os.getcwd()  
+#%%
+#identificamos los números de empleado duplicados
+# usamos unique para que muestre los valores únicos
+def eliminar_duplicados (dataframe,columna):
+    duplicados = dataframe[dataframe.duplicated(subset=[columna], keep=False)][columna].unique()
+    len(duplicados)
+
+    lista_duplicados= []
+
+    for numero in duplicados:
+        lista_duplicados.append(dataframe[dataframe[columna] == numero])
+
+    #Hay 104 numeros de empleado duplicados más NaN
+
+    # lo visualizamos en un nuevo dataFrame
+    df_resultado = pd.concat(lista_duplicados)
+
+    indices_a_sacar = df_resultado.iloc[::2, 0]
+
+    dataframe = dataframe.drop(indices_a_sacar)
+
+    return dataframe
+
+#%%
+def transformacion_datos (dataframe, columna, diccionario):
+    dataframe[columna]= dataframe[columna].replace(diccionario)
+
+    return dataframe
